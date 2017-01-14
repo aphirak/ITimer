973ea7d6ed62f4e7f6ba3ba0cc329c2d826b7a20
Chutiphon.k <chutiphon.k@ku.th>
2017-01-15 02:19:11 +0700

    :heavy_plus_sign: Connect wifi

--- timing-gate/src/main.ino
+++ timing-gate/src/main.ino
@@ -1,12 +1,35 @@
+#include <ESP8266WiFi.h>
+
 #define SWITCH 0
 #define LED 4
 
+const char* ssid     = "bach1";
+const char* password = "aaaaaaaaaa";
+
 int valueSwitch = 0;
+String macAddress = "";
 
 void setup() {
   Serial.begin(9600);
   pinMode(SWITCH, INPUT);
   pinMode(LED, OUTPUT);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC address: ");
+  macAddress = WiFi.macAddress();
+  Serial.println(macAddress);
 }
 
 void loop() {
