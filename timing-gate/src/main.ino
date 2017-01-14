4209e913eaf78ff6a2e4f40ce0445bd1bf7602dd
Chutiphon.k <chutiphon.k@ku.th>
2017-01-15 04:41:31 +0700

    :heavy_plus_sign: Connect mqtt

--- timing-gate/src/main.ino
+++ timing-gate/src/main.ino
@@ -1,14 +1,23 @@
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #define TICKLE 0
 #define LED_TICKLE 4
-#define LED_WIFI 5
+#define LED_CONNECTION 5
 
 const char* ssid     = "bach1";
 const char* password = "aaaaaaaaaa";
 
+#define mqtt_server "192.168.2.44"
+#define mqtt_port 1900
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 int valueTickle = 0;
-String macAddress = "";
+bool isDisable = false;
+String macAddress;
+// char* mqttTopic;
 
 void setup() {
 
@@ -16,7 +25,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(TICKLE, INPUT);
   pinMode(LED_TICKLE, OUTPUT);
-  pinMode(LED_WIFI, OUTPUT);
+  pinMode(LED_CONNECTION, OUTPUT);
   // ---------------------------------------------
 
   // -------------- Setup Wifi -------------------
@@ -35,28 +44,62 @@ void setup() {
   macAddress = WiFi.macAddress();
   Serial.println(macAddress);
   // ---------------------------------------------
-  
+
+  // -------------- Setup Mqtt -------------------
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  // ---------------------------------------------
+  // strcat(mqttTopic, "/TIMINGGATE/");
+  // strcat(mqttTopic, macAddress.c_str());
+  // strcat(mqttTopic, "/TICKLE");
+  // mqttTopic += "/";
+  // mqttTopic += macAddress;
+  // mqttTopic += "/TICKLE";
+  // Serial.println(mqttTopic);
 }
 
 void loop() {
 
-  // ---------- Check Connect Wifi ---------------
-  if(WiFi.status() == WL_CONNECTED){
-    digitalWrite(LED_WIFI, 1);
+  if(!client.connected()){
+    Serial.print("Attemping MQTT connecting...");
+    if(client.connect("TimingGate")){
+      Serial.println("connected");
+      client.subscribe("/TIMINGGATE");
+      digitalWrite(LED_CONNECTION, 1);
+      digitalWrite(LED_TICKLE, 1);
+    } else{
+      digitalWrite(LED_CONNECTION, 0);
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
   } else {
-    digitalWrite(LED_WIFI, 0);
+    // ------------- Check Tickle ------------------
+    valueTickle = digitalRead(TICKLE);
+    if(valueTickle && !isDisable){
+      isDisable = true;
+      Serial.println("Tickle");
+      client.publish("/TIMINGGATE/TICKLE", "1");
+      digitalWrite(LED_TICKLE, 0);
+    }
+    // ---------------------------------------------
   }
-  // ---------------------------------------------
+  client.loop();
+}
 
-  // ------------- Check Tickle ------------------
-  valueTickle = digitalRead(TICKLE);
-  if(valueTickle){
-    Serial.println("Click");
+void callback(char* topic, byte* payload, uint length){
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String msg = "";
+  int i = 0;
+  while (i<length) msg += (char)payload[i++];
+  if(msg == "RESET"){
+    isDisable = false;
     digitalWrite(LED_TICKLE, 1);
-  } else {
-    digitalWrite(LED_TICKLE, 0);
+    Serial.println(msg);
+    return;
   }
-  // ---------------------------------------------
-
-  delay(100);
+  Serial.println(msg);
 }
