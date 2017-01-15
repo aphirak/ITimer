07f33d5ddcfcdd6b7c5f05ba5152354c2f2501e3
Chutiphon.k <chutiphon.k@ku.th>
2017-01-15 23:12:36 +0700

    :wrench: :hammer: change function timer

--- gateway/timer.js
+++ gateway/timer.js
@@ -1,16 +1,44 @@
-var NanoTimer = require('nanotimer');
-var moment = require('moment')
+const mqtt = require('mqtt')
+const NanoTimer = require('nanotimer')
+const moment = require('moment')
+const timer = new NanoTimer()
 
-var y = moment.duration(0, 'milliseconds')
- 
-function main(){
-    var timer = new NanoTimer();
-    timer.setInterval(countDown, '', '1m');
+const options = {
+	host: '192.168.2.44',
+	port: 1900,
 }
- 
-function countDown(){
-	y = moment.duration(y + 1, 'milliseconds')
-	console.log(y.hours() + ":" + y.minutes() + ":" + y.seconds() + ":" + y.milliseconds())
+const client  = mqtt.connect(options)
+let timestamp = 0
+let result = []
+let isStart = false
+
+function countUp(){
+	timestamp = moment.duration(timestamp + 1, 'milliseconds')
 }
+
+client.on('connect', () => {
+  client.subscribe('/TIMINGGATE/TICKLE')
+  client.publish('/TIMINGGATE', 'Hello mqtt')
+})
+
+
  
-main();
+client.on('message', (topic, payload) => {
+  let msg = payload.toString()
+  if(!isStart){
+  	console.log('Start')
+  	isStart = true
+    timer.setInterval(countUp, '', '1m')
+  } else if(msg == "1"){
+	  	result.push(timestamp.asMilliseconds())  		
+	  	if(result.length == 4){
+		  	console.log('End')
+			console.log(result)
+			timer.clearInterval()
+		  	isStart = false
+			timestamp = 0
+			result = []
+	  	}
+	}
+})
+
