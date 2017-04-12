8bf223310d7667dd11ffda6088fc9c1e70e60ca7
Chutiphon.k <chutiphon.k@ku.th>
2017-04-13 01:26:38 +0700

    :heavy_plus_sign: Add connect socket

--- gateway_mock/src/bin/socket.js
+++ gateway_mock/src/bin/socket.js
@@ -8,18 +8,8 @@ let io
 sockets.init = (server) => {
 	io = new SocketIO(server)
 	io.on('connect', (socket) => {
-		let { gate, isStarted, isSetup, results, time, uid, nGate, distances, competitions } = state
-		socket.emit('timer', {
-			gate,
-			isSetup,
-			results,
-			time,
-			isStarted,
-			uid,
-			nGate,
-			distances
-		})
-		socket.emit('competitions', competitions)
+		socket.emit('timer', { ...state, competitions: undefined })
+		socket.emit('competitions', state.competition)
 	})
 }
 
