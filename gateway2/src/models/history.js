b3366d1bd49b3c13c5323ff3b66ccafa1f83e5e9
Chutiphon.k <chutiphon.k@ku.th>
2017-04-14 04:41:17 +0700

    :heavy_minus_sign: Remove gateway2

--- gateway2/src/models/history.js
+++
@@ -1,8 +0,0 @@
-import bookshelf from '../../bookshelf'
-
-const History = bookshelf.Model.extend({
-	tableName: 'histories',
-	hasTimestamps: true
-})
-
-export default History
--- gateway2/src/models/index.js
+++
@@ -1,2 +0,0 @@
-export User from './user'
-export History from './history'
