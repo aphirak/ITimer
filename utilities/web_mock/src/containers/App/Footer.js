5075be1a3c189a975090f8e87a4d4f008cf66dba
Chutiphon.k <chutiphon.k@ku.th>
2017-04-18 01:19:06 +0700

    :heavy_plus_sign: Add footer

--- utilities/web_mock/src/containers/App/Footer.js
+++ utilities/web_mock/src/containers/App/Footer.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
 
+import { logoWebpack,
+	logoRedux,
+	logoReact,
+	logoBulma
+} from 'assets/images'
+
 export default class Footer extends Component {
 	render () {
 		return (
-			<div>
-				Footer
-			</div>
+			<footer className="footer">
+				<div className="container">
+					<div className="content has-text-centered">
+						<p>
+							<strong>Development By</strong>
+						</p>
+						<div className="columns">
+							<div className="column is-4" />
+							<div className="column">
+								<figure className="image is-64x64">
+									<img src={logoReact} />
+								</figure>
+							</div>
+							<div className="column">
+								<figure className="image is-64x64">
+									<img src={logoRedux} />
+								</figure>
+							</div>
+							<div className="column">
+								<figure className="image is-64x64">
+									<img src={logoBulma} />
+								</figure>
+							</div>
+							<div className="column">
+								<figure className="image is-64x64">
+									<img src={logoWebpack} />
+								</figure>
+							</div>
+							<div className="column is-4" />
+						</div>
+					</div>
+				</div>
+			</footer>
 		)
 	}
 }
