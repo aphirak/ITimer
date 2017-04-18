7953519336a9fb0903f7689344f8f3806af20659
Chutiphon.k <chutiphon.k@ku.th>
2017-04-19 01:00:53 +0700

    :wrench: Change path knexfile

--- gateway/bookshelf.js
+++ gateway/bookshelf.js
@@ -2,7 +2,7 @@ import knex from 'knex'
 import bookshelf from 'bookshelf'
 import cascadeDelete from 'bookshelf-cascade-delete'
 
-import knexfile from './knexfile'
+import knexfile from 'root/knexfile'
 
 const dbConfig = knexfile.development
 const Bookshelf = bookshelf(knex(dbConfig))
